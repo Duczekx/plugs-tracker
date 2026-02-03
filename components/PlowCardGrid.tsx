5ebb003a6e186d07a9be1a7ece4513573ec06860
Duczekx <dawidgame97@gmail.com>
2026-02-03 19:18:03 +0100

    Fix plow tiles grid and orange box

--- components/PlowCardGrid.tsx
+++ components/PlowCardGrid.tsx
@@ -40,7 +40,7 @@ export default function PlowCardGrid({
   }
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {items.map((item) => (
         <div key={item.key} className="h-full">
           <PlowCard
